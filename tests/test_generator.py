ce5870be5c3660d4bc9a3b29fa09ce343b6fc3d4
ali92hm <ali@alihm.net>
2021-08-25 17:44:51 -0500

    Add basic tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,10 +1,18 @@
-
-def test_bake_project(cookies):
-    # result = cookies.bake(extra_context={"repo_name": "helloworld"})
+def test_bake_project_with_defaults(cookies):
     result = cookies.bake()
 
     assert result.exit_code == 0
     assert result.exception is None
 
-    assert result.project_path.name == "test_project"
+    assert result.project_path.name == "test-project"
+    assert result.project_path.is_dir()
+
+
+def test_bake_project_with_custom_input(cookies):
+    result = cookies.bake(extra_context={"project_name": "some Kool_project"})
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name == "some-kool-project"
     assert result.project_path.is_dir()
