8dfae33eb9af89c626fe179194515ee128cdce0d
ali92hm <ali@alihm.net>
2021-09-21 17:38:17 -0500

    Added pre and post gen hooks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,13 @@
+# import re
+# import sys
+
+
+# MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+
+# project_name = "{{ cookiecutter.project_name }}"
+
+# if not re.match(MODULE_REGEX, project_name):
+#     print("ERROR: %s is not a valid Python module name!" % project_name)
+
+#     # exits with status 1 to indicate failure
+#     sys.exit(1)
