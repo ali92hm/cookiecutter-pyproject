0bed309c036aed9671c674f330ac6873e23ca7b6
ali92hm <ali@alihm.net>
2021-09-22 17:51:56 -0500

    Prevent the private input variables to be set

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
 import re
-import sys
 
+# TODO: Remove this block
+# project_name_snake_case and project_name_kebab_case vairables while can be set by the
+# user, they are not meant to. In later versions of cookiecutter we can add __ prefix
+# to them in order to hide them from the user.
+# In the meantime, we till throw an exception if they are different than the project name
+project_name = "{{ cookiecutter.project_name_snake_case }}"
+project_name_snake_case = "{{ cookiecutter.project_name_snake_case }}"
+project_name_kebab_case = "{{ cookiecutter.project_name_kebab_case }}"
+
+if project_name.lower().replace(" ", "_").replace("-", "_") != project_name_snake_case:
+    raise Exception(
+        "Error: project_name_snake_case cannot be set. This input will be removed in the future"
+    )
+
+if project_name.lower().replace(" ", "-").replace("_", "-") != project_name_kebab_case:
+    raise Exception(
+        "Error: project_name_kebab_case cannot be set. This input will be removed in the future"
+    )
+
+# end block
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
-
-project_name_snake_case = "{{ cookiecutter._project_name_snake_case }}"
+project_name = "{{ cookiecutter.project_name_snake_case }}"
+project_name_snake_case = "{{ cookiecutter.project_name_snake_case }}"
 
 if not re.match(MODULE_REGEX, project_name_snake_case):
-    print(f"ERROR: {project_name_snake_case} is not a valid Python module name!")
-    sys.exit(1)
+    raise Exception(f"Error: {project_name} is not a valid Python module name!")
