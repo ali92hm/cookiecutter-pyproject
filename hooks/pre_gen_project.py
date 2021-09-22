61e4678c6dcea921ba3c1fa2756573a1440e8475
ali92hm <ali@alihm.net>
2021-09-22 16:20:05 -0500

    Renme files to use the new variables

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,7 +6,7 @@ import sys
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-project_name_snake_case = "{{ cookiecutter.project_name_snake_case }}"
+project_name_snake_case = "{{ cookiecutter._project_name_snake_case }}"
 
 if not re.match(MODULE_REGEX, project_name_snake_case):
     print(f"ERROR: {project_name_snake_case} is not a valid Python module name!")
