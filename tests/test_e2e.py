da658bdafce77fd02279fcd1b1fae82785151524
ali92hm <ali@alihm.net>
2021-09-22 15:23:20 -0500

    Add e2e tests

---
+++ tests/test_e2e.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def install_dep_and_run_ci(generated_project):
+    project_path = generated_project.project_path
+
+    subprocess.run("make init", cwd=project_path, shell=True, check=True)
+    subprocess.run("make ci", cwd=project_path, shell=True, check=True)
+    subprocess.run("make build", cwd=project_path, shell=True, check=True)
+    subprocess.run("make clean", cwd=project_path, shell=True, check=True)
+
+
+def test_e2e_defaults(cookies):
+    generated_project = cookies.bake()
+    install_dep_and_run_ci(generated_project)
