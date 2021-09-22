2bebed3a587672a6fa85f8bec0c4ccfd7bf808d5
ali92hm <ali@alihm.net>
2021-09-22 12:31:16 -0500

    Added pre and post gen hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,13 @@
-# nothing here yet
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+GENERATED_DIR = os.path.abspath(os.path.curdir)
+
+
+def remove_file(filepath: str) -> None:
+    os.remove(os.path.join(GENERATED_DIR, filepath))
+
+
+subprocess.run("git init", cwd=GENERATED_DIR, shell=True, check=True)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,13 @@
-# import re
-# import sys
+#!/usr/bin/env python
+
+import re
+import sys
 
 
-# MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-# project_name = "{{ cookiecutter.project_name }}"
+project_name_snake_case = "{{ cookiecutter.project_name_snake_case }}"
 
-# if not re.match(MODULE_REGEX, project_name):
-#     print("ERROR: %s is not a valid Python module name!" % project_name)
-
-#     # exits with status 1 to indicate failure
-#     sys.exit(1)
+if not re.match(MODULE_REGEX, project_name_snake_case):
+    print(f"ERROR: {project_name_snake_case} is not a valid Python module name!")
+    sys.exit(1)
