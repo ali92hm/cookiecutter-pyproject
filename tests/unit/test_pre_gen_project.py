c0b08ee210e2a410b6f5d9ab1b6f66c87e0a7925
ali92hm <ali@alihm.net>
2021-09-24 13:09:42 -0500

    split tests into seperate folders

---
+++ tests/unit/test_pre_gen_project.py
@@ -0,0 +1,7 @@
+# from hooks import pre_gen_project
+
+
+def test_name_should_succeed():
+    assert 2 == 2
+    # is_valid = pre_gen_project.is_validate_project_name("asdf123")
+    # assert is_valid
