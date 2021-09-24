976da947be43461a497efe01dd45bce019180e1e
ali92hm <ali@alihm.net>
2021-09-24 13:32:56 -0500

    Add scripts

---
+++ scripts/clean.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+source ./scripts/include/vars.sh
+
+rm -rf $GENERATED_PROJECTS_FOLDER
+rm -rf .mypy_cache
+rm -rf .pytest_cache
+rm -fr dist/
+rm -fr build/
+rm -rf *.egg-info/
+rm -rf *.egg
+rm -rf $REPORTS_FOLDER
+find . -name '*.pyc' -exec rm -f {} +
+find . -name '*.pyo' -exec rm -f {} +
+find . -name '*~' -exec rm -f {} +
+find . -name '__pycache__' -exec rm -fr {} +
