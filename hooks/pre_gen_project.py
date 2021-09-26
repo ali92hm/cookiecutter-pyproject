8722027bc837d1597d1c07296a468e53eef5ed64
ali92hm <ali@alihm.net>
2021-09-26 13:10:48 -0500

    Added tests

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -11,7 +11,7 @@ def get_project_name_snake_case(project_name: str) -> str:
     return project_name.lower().replace(" ", "_").replace("-", "_")
 
 
-def is_validate_project_name(project_name_snake_case: str) -> bool:
+def is_validate_python_project_name(project_name_snake_case: str) -> bool:
     MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
     if re.match(MODULE_REGEX, project_name_snake_case):
@@ -42,6 +42,8 @@ if __name__ == "__main__":
 
     # end block
 
-    if not is_validate_project_name("{{ cookiecutter.project_name_snake_case }}"):
+    if not is_validate_python_project_name(
+        "{{ cookiecutter.project_name_snake_case }}"
+    ):
         project_name = "{{ cookiecutter.project_name_snake_case }}"
         raise Exception(f"Error: {project_name} is not a valid Python module name!")
--- tests/unit/test_pre_gen_project.py
+++ tests/unit/test_pre_gen_project.py
@@ -1,7 +1,46 @@
-# from hooks import pre_gen_project
+import pytest
+
+from hooks import pre_gen_project
 
 
-def test_name_should_succeed():
-    assert 2 == 2
-    # is_valid = pre_gen_project.is_validate_project_name("asdf123")
-    # assert is_valid
+@pytest.mark.parametrize(
+    "name_input,expected",
+    [
+        ("a34akfi5", "a34akfi5"),
+        ("TeST proJect", "test-project"),
+        ("123TeST proJect123", "123test-project123"),
+        ("Test^&Project", "test^&project"),
+        ("Te_st-Pro_-jct", "te-st-pro--jct"),
+    ],
+)
+def test_get_project_name_kebab_case(name_input, expected):
+    assert pre_gen_project.get_project_name_kebab_case(name_input) == expected
+
+
+@pytest.mark.parametrize(
+    "name_input,expected",
+    [
+        ("a34akfi5", "a34akfi5"),
+        ("TeST proJect", "test_project"),
+        ("123TeST proJect123", "123test_project123"),
+        ("Test^&Project", "test^&project"),
+        ("Te_st-Pro_-jct", "te_st_pro__jct"),
+    ],
+)
+def test_get_project_name_snake_case(name_input, expected):
+    assert pre_gen_project.get_project_name_snake_case(name_input) == expected
+
+
+@pytest.mark.parametrize(
+    "name_input,expected",
+    [
+        ("a34akfi5", True),
+        ("TeST proJect", False),
+        ("123TeST proJect", False),
+        ("TeST proJect134", False),
+        ("Test^&Project", False),
+        ("Te_st-Pro_-jct", False),
+    ],
+)
+def test_is_validate_python_project_name(name_input, expected):
+    assert pre_gen_project.is_validate_python_project_name(name_input) == expected
