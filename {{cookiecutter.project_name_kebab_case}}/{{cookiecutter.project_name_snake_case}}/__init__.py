8a4064d1f4f246625b4a678ae36d5c9a0c0b1f88
ali92hm <ali@alihm.net>
2021-10-09 19:35:52 -0500

    Add project info to the init file

--- {{cookiecutter.project_name_kebab_case}}/{{cookiecutter.project_name_snake_case}}/__init__.py
+++ {{cookiecutter.project_name_kebab_case}}/{{cookiecutter.project_name_snake_case}}/__init__.py
@@ -1,3 +1,8 @@
+__title__ = "{{ cookiecutter.project_name_kebab_case }}"
+__description__ = "{{ cookiecutter.project_description }}"
+__url__ = "{{ cookiecutter.project_repo }}/blob/master/README.md"
 __author__ = "{{ cookiecutter.author_full_name }}"
 __email__ = "{{ cookiecutter.author_email }}"
+__copyright__ = "Copyright {% now 'local', '%Y' %} {{ cookiecutter.author_full_name }}"
+__license__ = "{{ cookiecutter.license }}"
 __version__ = "0.1.0"
