416eaa03952e02b73f7a0b1a485a8b0f9a37b7e4
ali92hm <ali@alihm.net>
2021-12-14 17:22:01 -0800

    Remove license file if project is not open source

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,10 @@ import subprocess
 
 if __name__ == "__main__":
     GENERATED_DIR = os.path.abspath(os.path.curdir)
+
+    license = "{{ cookiecutter.license }}"
+
+    if license == "Not open source":
+        subprocess.run("rm LICENSE", cwd=GENERATED_DIR, shell=True, check=True)
+
     subprocess.run("git init", cwd=GENERATED_DIR, shell=True, check=True)
