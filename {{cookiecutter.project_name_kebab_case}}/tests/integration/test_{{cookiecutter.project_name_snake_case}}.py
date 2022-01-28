84eca6d8b42266d910be4c5cfe77511375124abf
ali92hm <ali@alihm.net>
2022-01-27 22:28:39 -0800

    Fix formatting issues

--- {{cookiecutter.project_name_kebab_case}}/tests/integration/test_{{cookiecutter.project_name_snake_case}}.py
+++ {{cookiecutter.project_name_kebab_case}}/tests/integration/test_{{cookiecutter.project_name_snake_case}}.py
@@ -2,11 +2,12 @@ import pytest
 
 from {{ cookiecutter.project_name_snake_case }} import {{ cookiecutter.project_name_snake_case }}
 
-class {{ cookiecutter.project_name_snake_case.capitalize() }}Test:
+
+class Test{{ cookiecutter.project_name_snake_case.capitalize().replace('_', '') }}:
     def test_add_unit(self):
         res = {{ cookiecutter.project_name_snake_case }}.add(2, 3)
         assert res == 5
 
-    @pytest.mark.skip(reason='Test skiping test')
+    @pytest.mark.skip(reason="Test skiping test")
     def test_skip(self):
         assert False
