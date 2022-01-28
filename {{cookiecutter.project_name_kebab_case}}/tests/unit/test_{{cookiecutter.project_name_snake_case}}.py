b1a5852ea6f4165192a5515f3eb8c5b67bede006
ali92hm <ali@alihm.net>
2022-01-27 22:09:07 -0800

    Use classes for tests

--- {{cookiecutter.project_name_kebab_case}}/tests/unit/test_{{cookiecutter.project_name_snake_case}}.py
+++ {{cookiecutter.project_name_kebab_case}}/tests/unit/test_{{cookiecutter.project_name_snake_case}}.py
@@ -1,6 +1,12 @@
+import pytest
+
 from {{ cookiecutter.project_name_snake_case }} import {{ cookiecutter.project_name_snake_case }}
 
+class {{ cookiecutter.project_name_snake_case.capitalize() }}Test:
+    def test_add_unit(self):
+        res = {{ cookiecutter.project_name_snake_case }}.add(2, 3)
+        assert res == 5
 
-def test_add_unit():
-    res = {{ cookiecutter.project_name_snake_case }}.add(2, 3)
-    assert res == 5
+    @pytest.mark.skip(reason='Test skiping test')
+    def test_skip(self):
+        assert False
